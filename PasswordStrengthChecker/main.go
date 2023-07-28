50e7a9a0fa26bc7e862e182613a58da43dabf06a
zachc4614 <91169352+zachc4614@users.noreply.github.com>
2023-07-28 14:40:47 -0400

    Update main.go

--- PasswordStrengthChecker/main.go
+++ PasswordStrengthChecker/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+	"math"
+	"unicode"
 )
 
 func main() {
@@ -12,6 +14,14 @@ func main() {
 	password := readPassword()
 	strengthScore := calculatePasswordStrength(password)
 	strengthRating := getPasswordStrengthRating(strengthScore)
+
+	if strengthScore <= 1 {
+		suggestions := getPasswordSuggestions(password)
+		fmt.Println("Password is weak. Consider the following suggestions:")
+		for _, suggestion := range suggestions {
+			fmt.Println("-", suggestion)
+		}
+	}
 
 	fmt.Printf("Password Strength: %s\n", strengthRating)
 }
@@ -64,7 +74,29 @@ func hasNumber(password string) bool {
 
 func hasSpecialCharacter(password string) bool {
 	specialCharacters := "!@#$%^&*()_+{}[]:;<>,.?/~`"
-	return strings.ContainsAny(password, specialCharacters)
+	leetCharacters := "4bcd3fgh1jklmn0pqr57uvwxy2"
+
+	// Check for special characters
+	for _, char := range password {
+		if strings.ContainsRune(specialCharacters, char) {
+			return true
+		}
+	}
+
+	// Check for leet speak substitutions
+	for _, char := range password {
+		if unicode.IsLetter(char) {
+			leetIndex := int(unicode.ToLower(char)) - 'a'
+			if leetIndex >= 0 && leetIndex < len(leetCharacters) {
+				leetEquivalent := rune(leetCharacters[leetIndex])
+				if strings.ContainsRune(password, leetEquivalent) {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
 }
 
 func getPasswordStrengthRating(strength int) string {
@@ -78,4 +110,79 @@ func getPasswordStrengthRating(strength int) string {
 	default:
 		return "Very Strong"
 	}
+}
+
+func calculateEntropy(password string) float64 {
+	charsetSize := 0
+	entropy := 0.0
+
+	if hasUpperCaseLetter(password) {
+		charsetSize += 26
+		entropy += math.Log2(26)
+	}
+
+	if hasLowerCaseLetter(password) {
+		charsetSize += 26
+		entropy += math.Log2(26)
+	}
+
+	if hasNumber(password) {
+		charsetSize += 10
+		entropy += math.Log2(10)
+	}
+
+	if hasSpecialCharacter(password) {
+		charsetSize += 32 // Assuming 32 possible special characters
+		entropy += math.Log2(32)
+	}
+
+	return float64(charsetSize) * entropy
+}
+
+func getPasswordSuggestions(password string) []string {
+	var suggestions []string
+
+	if len(password) < 8 {
+		suggestions = append(suggestions, "Use at least 8 characters")
+	}
+
+	if !hasUpperCaseLetter(password) {
+		suggestions = append(suggestions, "Include at least one uppercase letter")
+	}
+
+	if !hasLowerCaseLetter(password) {
+		suggestions = append(suggestions, "Include at least one lowercase letter")
+	}
+
+	if !hasNumber(password) {
+		suggestions = append(suggestions, "Include at least one number")
+	}
+
+	if !hasSpecialCharacter(password) {
+		suggestions = append(suggestions, "Include at least one special character")
+	}
+
+	if isCommonPassword(password) {
+		suggestions = append(suggestions, "Avoid using common or easily guessable passwords")
+	}
+
+	return suggestions
+}
+
+func isCommonPassword(password string) bool {
+	commonPasswords := []string{
+		"password", "123456", "qwerty", "admin", "letmein", "monkey", "abc123",
+		"iloveyou", "123123", "password1", "qwertyuiop", "welcome", "login",
+		"password123", "12345", "sunshine", "1234567890", "princess", "admin123",
+		"password!@#", "baseball", "password1", "dragon", "trustno1",
+	}
+
+	password = strings.ToLower(password)
+	for _, commonPwd := range commonPasswords {
+		if password == commonPwd {
+			return true
+		}
+	}
+
+	return false
 }
