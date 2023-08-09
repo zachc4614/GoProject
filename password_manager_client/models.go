d5da7ea241a6184b623e0f3894682b4c85fdf404
zachc4614 <91169352+zachc4614@users.noreply.github.com>
2023-08-09 11:43:57 -0400

    Update models.go

--- password_manager_client/models.go
+++ password_manager_client/models.go
@@ -1,28 +1,67 @@
 package main
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+)
 
+// User represents user information
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// GetUserByUsername retrieves user by username
+func GetUserByUsername(username string) (*User, error) {
+	// Implement your logic to retrieve user from storage (e.g., database)
+	// Return user and nil if found, or nil and an error if not found
+	return nil, nil
+}
+
+// CreateUser stores a new user
+func CreateUser(username, password string) error {
+	// Hash the password before storing
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	// Implement your logic to store new user in storage (e.g., database)
+	return nil
+}
+
+// AuthenticateUser authenticates user credentials
+func AuthenticateUser(username, password string) (bool, error) {
+	user, err := GetUserByUsername(username)
+	if err != nil {
+		return false, err
+	}
+
+	if user == nil {
+		return false, nil
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+// Configuration represents the configuration information
 type Configuration struct {
 	Username     string `json:"username"`
 	SymmetricKey string `json:"symmetric_key"`
 }
 
-type DatabaseData struct {
-	Description   string `json:"description"`
-	Username      string `json:"username"`
-	EncryptedData string `json:"encrypted_data"`
-}
-
-type AccountInfo struct {
-	Description string `json:"description"`
-	Username    string `json:"username"`
-	Password    string `json:"password"`
-}
-
+// ToJSON converts Configuration struct to JSON
 func (c *Configuration) ToJSON() ([]byte, error) {
 	return json.MarshalIndent(c, "", "  ")
 }
 
+// ConfigurationFromJSON parses JSON to create Configuration struct
 func ConfigurationFromJSON(data []byte) (*Configuration, error) {
 	var c Configuration
 	err := json.Unmarshal(data, &c)
@@ -32,10 +71,19 @@ func ConfigurationFromJSON(data []byte) (*Configuration, error) {
 	return &c, nil
 }
 
+// DatabaseData represents data stored in the database
+type DatabaseData struct {
+	Description   string `json:"description"`
+	Username      string `json:"username"`
+	EncryptedData string `json:"encrypted_data"`
+}
+
+// ToJSON converts DatabaseData struct to JSON
 func (d *DatabaseData) ToJSON() ([]byte, error) {
 	return json.MarshalIndent(d, "", "  ")
 }
 
+// DatabaseDataFromJSON parses JSON to create DatabaseData struct
 func DatabaseDataFromJSON(data []byte) (*DatabaseData, error) {
 	var d DatabaseData
 	err := json.Unmarshal(data, &d)
@@ -45,10 +93,19 @@ func DatabaseDataFromJSON(data []byte) (*DatabaseData, error) {
 	return &d, nil
 }
 
+// AccountInfo represents account information
+type AccountInfo struct {
+	Description string `json:"description"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+}
+
+// ToJSON converts AccountInfo struct to JSON
 func (a *AccountInfo) ToJSON() ([]byte, error) {
 	return json.MarshalIndent(a, "", "  ")
 }
 
+// AccountInfoFromJSON parses JSON to create AccountInfo struct
 func AccountInfoFromJSON(data []byte) (*AccountInfo, error) {
 	var a AccountInfo
 	err := json.Unmarshal(data, &a)
