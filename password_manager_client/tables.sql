1df0c7b20504356d13a8129502fcc94b33db0292
zachc4614 <91169352+zachc4614@users.noreply.github.com>
2023-08-15 13:34:18 -0400

    Update tables.sql

--- password_manager_client/tables.sql
+++ password_manager_client/tables.sql
@@ -1,12 +1,14 @@
 CREATE TABLE users (
-	username VARCHAR(50) PRIMARY KEY,
-	password VARCHAR(255) NOT NULL
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE accounts (
-	description VARCHAR(255),
-	username VARCHAR(50),
-	encrypted_data TEXT,
-	FOREIGN KEY (username) REFERENCES users(username)
+    id SERIAL PRIMARY KEY,                   -- Added a primary key for the accounts table
+    description VARCHAR(255) NOT NULL,
+    username VARCHAR(50) NOT NULL,           -- Made the username NOT NULL
+    encrypted_data TEXT NOT NULL,            -- Made the encrypted_data NOT NULL
+    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE  -- Added ON DELETE CASCADE
 );
 
+CREATE INDEX idx_accounts_username ON accounts(username);   -- Added an index on the username column for faster lookups
