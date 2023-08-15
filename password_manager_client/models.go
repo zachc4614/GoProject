8d667c299a0ecba6be981e0887e9d6374fc7ef38
zachc4614 <91169352+zachc4614@users.noreply.github.com>
2023-08-15 13:33:43 -0400

    Update models.go

--- password_manager_client/models.go
+++ password_manager_client/models.go
@@ -4,52 +4,56 @@ import (
 	"errors"
 	"log"
 	"os"
-
 	"golang.org/x/crypto/bcrypt"
+	"database/sql"
 )
 
 var modelsLogger *log.Logger
-var userStore map[string]string // In-memory store for users
 
 func init() {
 	modelsLogger = log.New(os.Stdout, "[Models] ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
-	initStore()
-}
-
-func initStore() {
-	userStore = make(map[string]string)
 }
 
 func GetUserByUsername(username string) (*User, error) {
 	modelsLogger.Printf("Retrieving user by username: %s", username)
-	if hashedPassword, found := userStore[username]; found {
-		return &User{Username: username, Password: hashedPassword}, nil
+	
+	var hashedPassword string
+	err := db.QueryRow("SELECT password FROM users WHERE username = $1", username).Scan(&hashedPassword)
+	
+	if err == sql.ErrNoRows {
+		return nil, errors.New("authentication failed")
+	} else if err != nil {
+		return nil, err
 	}
-	return nil, errors.New("user not found")
+
+	return &User{Username: username, Password: hashedPassword}, nil
 }
 
 func CreateUser(username, password string) error {
 	modelsLogger.Printf("Creating new user: %s", username)
+
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		modelsLogger.Printf("Error generating password hash: %v", err)
 		return err
 	}
-	userStore[username] = string(hashedPassword)
+
+	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", username, string(hashedPassword))
+	if err != nil {
+		modelsLogger.Printf("Error inserting new user into database: %v", err)
+		return err
+	}
+
 	return nil
 }
 
 func AuthenticateUser(username, password string) (bool, error) {
 	modelsLogger.Printf("Authenticating user: %s", username)
+
 	user, err := GetUserByUsername(username)
 	if err != nil {
 		modelsLogger.Printf("Error retrieving user: %v", err)
 		return false, err
-	}
-
-	if user == nil {
-		modelsLogger.Printf("User not found: %s", username)
-		return false, nil
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
