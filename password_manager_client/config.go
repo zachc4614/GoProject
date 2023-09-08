ee0e1952aa98cc47d30e7709a694b40022e62043
zachc4614 <91169352+zachc4614@users.noreply.github.com>
2023-09-08 14:49:13 -0400

    Add files via upload

---
+++ password_manager_client/config.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+    dbHost     = "your-db-host"
+    dbPort     = 5432
+    dbUser     = "your-db-username"
+    dbPassword = "your-db-password"
+    dbName     = "your-db-name"
+)
